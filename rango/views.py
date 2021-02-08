b8b90a5e735235156e5d18114cc91bada214fc2d
“Jan <2479153c@student.gla.ac.uk>
2021-02-08 19:05:11 +0000

    Chapter 9 finished

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from rango.models import Category, Page
 from django.http import HttpResponse
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -46,6 +48,7 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context = context_dict)
 
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -63,6 +66,7 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -126,6 +130,33 @@ def register(request):
                   context = {'user_form': user_form,
                              'profile_form': profile_form,
                              'registered': registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
 
 
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -88,6 +88,8 @@ DATABASES = {
     }
 }
 
+LOGIN_URL = 'rango:login'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
